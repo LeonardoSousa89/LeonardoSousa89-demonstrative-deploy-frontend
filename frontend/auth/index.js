52a4cfa8f09f3bc2e920091d98b931d26011496b
Leozin89python <leozin069@gmail.com>
2021-08-12 16:49:55 -0300

    deploy

--- frontend/auth/index.js
+++ frontend/auth/index.js
@@ -1,52 +1,19 @@
-document.onclick = addEventListener('click',(e)=> e.preventDefault())
-document.onload  = addEventListener('load', ( )=>{
-    $('#alerting').hide(1)
-    $('#empty').hide(1)
-    $('#nofetch').hide(1)
-})  
+//metodos provisórios
 $('#button').click(function(){
     let admInput  = document.querySelector('#admin').value
     let passInput = document.querySelector('#pass').value 
-    let url = 'http://localhost:3004/auth'
+	
+	if(admInput  == 'mendes' && passInput == '1234' ||
+	   admInput  == 'admin' && passInput == 'admin'){
+	   document.location.href = 'auth/index.html'
+	}else if(admInput  != 'mendes' || passInput != '1234' ||
+	         admInput  != 'admin'  || passInput != 'admin'){
+		alert('invalid data!')
+	}else if(admInput  == ''  || passInput == ''){
+		alert('insert into all field values!')
+	}		
+}	
 
-    if(admInput == '' || passInput == ''){
-        $('#empty').show(100)
-        clear()
-    }else{
-        const con = {
-            method :'GET',
-            cache  :'default',
-            status :200
-        }
-
-        fetch(url,con)
-                .then(data => data.json())
-                .then(data => {
-                    data.map(e => {
-                        if(admInput == e.user && passInput == e.password){
-                            document.location.href = 'web/index.html'
-                        }else{
-                            $('#alerting').show(100) 
-                            clear()
-                        }
-                    })
-                })
-                .catch(_ => {
-                    $('#nofetch').show(100)
-                })
-    }
-})
-$('#admin').mouseenter(function(){
-    $('#alerting').hide(100)
-    $('#empty').hide(100)
-})
-$('#pass').mouseenter(function(){
-    $('#alerting').hide(100)
-    $('#empty').hide(100)
-})
-$('#nofetch').click(function(){
-    $(this).hide(100)
-})
 function clear(){
     let admInput  = document.querySelector('#admin')
     let passInput = document.querySelector('#pass')
